584769bfff7b4c0781c6b8d3f1a1c528bb15fb9f
teresacabell <teresacabll@gmail.com>
2022-03-09 13:51:31 -0600

    upload pizza data

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,47 +1,49 @@
-const { Schema, model } = require("mongoose");
-const dateFormat = require("../utils/dateFormat");
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema(
   {
     pizzaName: {
-      type: String,
+      type: String
     },
     createdBy: {
-      type: String,
+      type: String
     },
     createdAt: {
       type: Date,
       default: Date.now,
-      get: (createdAtVal) => dateFormat(createdAtVal)
+      get: createdAtVal => dateFormat(createdAtVal)
     },
     size: {
       type: String,
-      default: "Large",
+      default: 'Large'
     },
     toppings: [],
     comments: [
       {
         type: Schema.Types.ObjectId,
-        ref: "Comment",
-      },
-    ],
+        ref: 'Comment'
+      }
+    ]
   },
   {
     toJSON: {
       virtuals: true,
       getters: true
     },
-    id: false,
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
   }
 );
 
 // get total count of comments and replies on retrieval
-PizzaSchema.virtual("commentCount").get(function () {
-  return this.comments.length;
+PizzaSchema.virtual('commentCount').get(function() {
+  return this.comments.reduce(
+    (total, comment) => total + comment.replies.length + 1,
+    0
+  );
 });
 
-// create the Pizza model using the PizzaSchema
-const Pizza = model("Pizza", PizzaSchema);
+const Pizza = model('Pizza', PizzaSchema);
 
-// export the Pizza model
 module.exports = Pizza;
